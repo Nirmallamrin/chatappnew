838ad4561d4d677bb16ecdb0c4a88ce6cc7b9e7d
[nirmal] <nirmallamrin77@gmail.com>
2025-03-14 19:07:37 +0530

    final comitzz

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,9 +18,14 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+  })
+);
 
-app.use("/auth", authRoute)
+app.use("/auth", authRoute);
 app.use("/messages", messageRoute);
 
 app.get("/", (req, res) => {
@@ -28,5 +33,5 @@ app.get("/", (req, res) => {
 })
 
 server.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+    console.log(`server is running on port ${ PORT }`);
 })
